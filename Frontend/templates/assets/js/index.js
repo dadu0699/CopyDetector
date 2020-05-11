823b925e6ee816e9a231601bae914702d2bab8e8
Didier Domínguez <dadu0699@hotmail.com>
2020-05-10 21:59:18 -0600

    [UPDATE] Interface

--- Frontend/templates/assets/js/index.js
+++ Frontend/templates/assets/js/index.js
@@ -1,4 +1,4 @@
-var editor = CodeMirror.fromTextArea(document.getElementById('principalEditor'), {
+var principalEditor = CodeMirror.fromTextArea(document.getElementById('principalEditor'), {
     theme: 'dracula',
     mode: "text/x-java",
     lineNumbers: true,
@@ -8,3 +8,37 @@ var editor = CodeMirror.fromTextArea(document.getElementById('principalEditor'),
     autoCloseBrackets: true,
     matchBrackets: true
 });
+
+var comparatorEditor = CodeMirror.fromTextArea(document.getElementById('comparatorEditor'), {
+    theme: 'dracula',
+    mode: "text/x-java",
+    lineNumbers: true,
+    lineWrapping: false,
+    foldGutter: true,
+    gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter', 'CodeMirror-lint-markers'],
+    autoCloseBrackets: true,
+    matchBrackets: true
+});
+
+var treeEditor = CodeMirror.fromTextArea(document.getElementById('treeEditor'), {
+    theme: 'dracula',
+    mode: 'application/json',
+    lineNumbers: true,
+    lineWrapping: false,
+    foldGutter: true,
+    gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter', 'CodeMirror-lint-markers'],
+    autoCloseBrackets: true,
+    matchBrackets: true,
+    lint: true,
+    readOnly: true
+});
+
+let treeify = window.jsonTreeify;
+
+let testData = {
+    a: 1,
+    b: {
+        c: "hello world"
+    }
+};
+treeEditor.getDoc().setValue(treeify(testData));
