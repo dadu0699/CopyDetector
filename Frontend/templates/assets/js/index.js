0fbae02a934866e84e3a3a68f0141b9f3529b110
Didier Domínguez <dadu0699@hotmail.com>
2020-05-12 22:39:37 -0600

    [UPDATE]  User interface

--- Frontend/templates/assets/js/index.js
+++ Frontend/templates/assets/js/index.js
@@ -20,10 +20,30 @@ var comparatorEditor = CodeMirror.fromTextArea(document.getElementById('comparat
     matchBrackets: true
 });
 
+var console = CodeMirror.fromTextArea(document.getElementById('console'), {
+    theme: 'dracula',
+    lineWrapping: false,
+    foldGutter: true,
+    gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter', 'CodeMirror-lint-markers'],
+    autoCloseBrackets: true,
+    matchBrackets: true,
+    lint: true,
+    readOnly: true
+});
+
+$('div#AST').jstree({
+    'core': {
+        'themes': {
+            'name': 'proton',
+            'responsive': true
+        }
+    },
+    "plugins": ["contextmenu"]
+});
+
+/*
 var treeEditor = CodeMirror.fromTextArea(document.getElementById('treeEditor'), {
     theme: 'dracula',
-    mode: 'application/json',
-    lineNumbers: true,
     lineWrapping: false,
     foldGutter: true,
     gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter', 'CodeMirror-lint-markers'],
@@ -34,11 +54,10 @@ var treeEditor = CodeMirror.fromTextArea(document.getElementById('treeEditor'), 
 });
 
 let treeify = window.jsonTreeify;
-
 let testData = {
     a: 1,
     b: {
         c: "hello world"
     }
 };
-treeEditor.getDoc().setValue(treeify(testData));
+treeEditor.getDoc().setValue(treeify(testData));*/
