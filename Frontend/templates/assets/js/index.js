42331bf597cf02db5da5b16c8dbb137d4e7b11c8
Didier Domínguez <dadu0699@hotmail.com>
2020-05-16 16:08:18 -0600

    [UPDATE] Interface

--- Frontend/templates/assets/js/index.js
+++ Frontend/templates/assets/js/index.js
@@ -1,3 +1,11 @@
+/* Global variables */
+var astData = '<ul><li>Root node <ul><li>Child node 1</li><li>Child node 2</li><li>Child node 3</li><li>Child node 4</li></ul></li></ul>';
+var fileContent;
+var errorList;
+/* END -- Global variables */
+
+
+
 /* Component initialization */
 var principalEditor = CodeMirror.fromTextArea(document.getElementById('principalEditor'), {
     theme: 'dracula',
@@ -31,8 +39,9 @@ var consoleEditor = CodeMirror.fromTextArea(document.getElementById('consoleEdit
     lint: true,
     readOnly: true
 });
-
 /* END -- Component initialization */
+
+
 
 /* Upload and read file */
 document.getElementById('input-principalFile').addEventListener('change', getPrincipalFile);
@@ -72,8 +81,9 @@ function readFileContent(file) {
 }
 /* END -- Upload and read file */
 
+
+
 /* AST */
-var astData = '<ul><li>Root node <ul><li>Child node 1</li><li>Child node 2</li><li>Child node 3</li><li>Child node 4</li></ul></li></ul>'
 document.getElementById('astButton').onclick = function () {
     $('#astDiv').html('');
     $('#astDiv').prepend('<div class="astPanel" id="AST">' + astData + '</div>');
@@ -86,8 +96,83 @@ document.getElementById('astButton').onclick = function () {
         },
         "plugins": ["contextmenu"]
     });
-};
+}
 /* END -- AST */
+
+
+
+/* Download files */
+document.getElementById('saveFile').onclick = function () {
+    var data = principalEditor.getDoc().getValue();
+    writeContent(data, 'mainFile.java', 'text/java');
+}
+
+document.getElementById('saveFileCompare').onclick = function () {
+    var data = comparatorEditor.getDoc().getValue();
+    writeContent(data, 'comparedFile.java', 'text/java');
+}
+
+document.getElementById('errorReport').onclick = function () {
+    generateErrorReport();
+}
+/* END -- Download files */
+
+
+
+/* Create download file */
+function writeContent(content, fileName, contentType) {
+    var a = document.createElement('a');
+    var file = new Blob([content], {
+        type: contentType
+    });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+};
+/* END -- Create download file */
+
+
+/* HTML report structure */
+function header() {
+    fileContent = '';
+    fileContent += '<!doctype html>';
+    fileContent += '<html lang=\"es\">';
+    fileContent += '<head>';
+    fileContent += '<meta charset=\"utf - 8\">';
+    fileContent += '<meta name=\"viewport\" content=\"width = device - width, initial - scale = 1, shrink - to - fit = no\">';
+    fileContent += '<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css\">';
+    fileContent += '<link rel=\"stylesheet\" href=\"https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css\">';
+}
+
+function footer() {
+    fileContent += '<script src=\"https://code.jquery.com/jquery-3.3.1.js\"></script>';
+    fileContent += '<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\"></script>';
+    fileContent += '<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\"></script>';
+    fileContent += '<script src=\"https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js\"></script>';
+    fileContent += '<script src=\"https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js\"></script>';
+    fileContent += '<script> $(document).ready(function () {$(\'#example\').DataTable();});</script>';
+    fileContent += '</body>';
+    fileContent += '</html>';
+}
+
+function generateErrorReport() {
+    header();
+    fileContent += '<title>Errors</title>';
+    fileContent += '</head>';
+    fileContent += '<body>';
+    fileContent += '<div class=\"container\"><br>';
+    fileContent += '<h1>List of Errors</h1><hr>';
+    fileContent += '<table id=\"example\" class=\"table table - striped table - bordered\" style=\"width: 100 % \">';
+    fileContent += '<thead><tr><th>#</th><th>Error</th><th>Type</th><th>Description</th><th>Row</th><th>Column</th></tr></thead>';
+    fileContent += '<tbody>';
+
+    fileContent += '</tbody>';
+    fileContent += '</table>';
+    fileContent += '</div>';
+    footer();
+    writeContent(fileContent, 'errorList.html', 'text/html');
+}
+/* END -- HTML report structure */
 
 
 /*
